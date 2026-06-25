ba6691a04a9df8638bd35dce3c647e965f137b92
ajaykaravya <ajayb.karavyasolutions@gmail.com>
2026-06-25 13:27:27 +0530

    update page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,22 @@
 "use client";
 
-import { useState } from "react";
-import { supabase } from "@/lib/supabase";
-
 export default function Home() {
-  const [time, setTime] = useState<number | null>(null);
-  const [count, setCount] = useState<number | null>(null);
 
   async function testQuery() {
-
     const start = performance.now();
 
-    const { data, error } = await supabase.from("users").select("*").limit(50);
+    const res = await fetch("/api/test");
+    const data = await res.json();
 
     const end = performance.now();
 
-    console.log("Rows:", data?.length);
+    console.log("Rows:", data.length);
     console.log("Time:", Math.round(end - start), "ms");
-    console.log(error);
   }
 
   return (
-    <main style={{ padding: 20 }}>
-      <button onClick={testQuery}>Test Supabase Query</button>
-
-      {time && (
-        <div>
-          <p>Rows: {count}</p>
-          <p>Time: {time} ms</p>
-        </div>
-      )}
-    </main>
+    <button onClick={testQuery}>
+      Test Prisma Query
+    </button>
   );
 }
